956dad76496c773d55fc2017d0cfa2561f712e0e
Cabaña Federico <federicogabriel12@hotmail.com>
2025-01-22 13:19:52 -0300

    Estilo hover retirado para modelo responsivo

--- src/components/ContactIcon.tsx
+++ src/components/ContactIcon.tsx
@@ -1,60 +1,48 @@
-import { FaGithub, FaLinkedin, FaPhone } from "react-icons/fa";
-import React from "react";
+import { FaGithub, FaLinkedin, FaPhone } from 'react-icons/fa';
+import React from 'react';
 import style from "../styles/init.module.css";
 
 const ContactIcon: React.FC = () => {
-  const phoneNumber = "+542804708799";
-  const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+    const phoneNumber = "+5402804708799";
+    const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+    return(
+        <div className={style.iconsContainer}>
+            {/* Ícono de GitHub */}
+            <a href="https://github.com/elfefedelagente" target="_blank" rel="noopener noreferrer">
+            <FaGithub 
+                size={24} 
+                color="#FFFFFF" 
+                style={{ cursor: 'pointer' }} 
+            />
+            </a>
 
-  // Función para quitar el efecto hover después de hacer clic
-  const removeHoverEffect = (event: React.MouseEvent<HTMLAnchorElement>) => {
-    const target = event.currentTarget;
-    target.classList.remove(style.hoverEffect); // Asegúrate de tener esta clase en tu CSS
-    setTimeout(() => target.classList.add(style.hoverEffect), 300); // Reaplica el efecto después
-  };
-
-  return (
-    <div className={style.iconsContainer}>
-      {/* Ícono de GitHub */}
-      <a
-        href="https://github.com/elfefedelagente"
-        target="_blank"
-        rel="noopener noreferrer"
-        onClick={removeHoverEffect}
-      >
-        <FaGithub size={24} color="#FFFFFF" style={{ cursor: "pointer" }} />
-      </a>
-
-      {/* Ícono de LinkedIn */}
-      <a
-        href="https://www.linkedin.com/in/federico-caba%C3%B1a-17354822a/"
-        target="_blank"
-        rel="noopener noreferrer"
-        onClick={removeHoverEffect}
-      >
-        <FaLinkedin size={24} color="#0077b5" style={{ cursor: "pointer" }} />
-      </a>
-
-      {isMobile ? (
-        <a
-          href={`tel:${phoneNumber}`}
-          rel="noopener noreferrer"
-          onClick={removeHoverEffect}
-        >
-          <FaPhone size={24} color="#34C759" style={{ cursor: "pointer" }} />
-        </a>
-      ) : (
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <FaPhone
-            size={24}
-            color="#34C759"
-            style={{ cursor: "default", marginRight: "8px" }}
-          />
-          <span style={{ color: "#FFFFFF" }}>{phoneNumber}</span>
+            {/* Ícono de LinkedIn */}
+            <a href="https://www.linkedin.com/in/federico-caba%C3%B1a-17354822a/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin 
+                    size={24} 
+                    color="#0077b5" 
+                    style={{ cursor: 'pointer' }} 
+                />
+            </a>
+            {isMobile ? (
+                <a href={`tel:${phoneNumber}`} rel="noopener noreferrer">
+                    <FaPhone 
+                        size={24} 
+                        color="#34C759" 
+                        style={{ cursor: 'pointer' }} 
+                    />
+                </a>
+            ) : (
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <FaPhone 
+                        size={24} 
+                        color="#34C759" 
+                        style={{ cursor: 'default', marginRight: '8px' }} 
+                    />
+                    <span style={{ color: '#FFFFFF' }}>{phoneNumber}</span>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
-
 export default ContactIcon;
